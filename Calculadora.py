8039ba5c54c1104646a6aab62b60dfe6d811139f
Carlos Alejandro Hernandez <parale23@hotmil.com>
2017-10-03 22:25:36 -0500

    Se corrigio la classe calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,28 +1,19 @@
 # Carlos A. Hernandez
 class Calculadora():
-	
 	def __init__(self):
 		self.resultado= 0
 	try:
 		def obtener_resultado(self):
 			return self.resultado
-
-		def suma(self, num1, num2):
+		def suma (self, num1, num2):
 			self.resultado = num1 + num2
-
 		def resta (self, num1 , num2):
 			self.resultado = num1 - num2
-
 		def multiplicacion (self, num1 , num2):
 			self.resultado = num1 * num2
-
 		def divicion (self, num1 , num2):
 			self.resultado = num1 / num2
-
 		def potencia (self, num1 , num2):
 			self.resultado = num1 * num2
-
 	except:
 		print "Datos incorrectos"
-	
-
